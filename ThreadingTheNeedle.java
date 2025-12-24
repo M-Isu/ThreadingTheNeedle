cc10c41ee7e1785e2f7ca21f8115123009c4a00e
Mohammed <M.MOHAMMED@ismartghana.com>
2025-12-23 18:52:34 -0800

    first section of code

--- ThreadingTheNeedle.java
+++ ThreadingTheNeedle.java
@@ -1,20 +1,39 @@
 import java.util.*;
 
-class ThreadingTheNeedle{
+public class ThreadingTheNeedle{
 
-
-public static void main(String args[]){
+public static void main(String args[]) throws Exception{
 			something myobject = new something();
 			
 			//myobject.start();
 			
 			//check the state of the already created, 
 			
-			System.out.println(myobject.state());
+
+
+			myobject.start();
+
+			//Boolean variable = null;
+
+
+			Boolean variable = (myobject.getState().toString().contains("NEW,RUNNING")) ? true : false;
+
+			System.out.println((variable) ? "Test thread is either new , or running": "Test thread ended");
+
+			while(variable){
+				System.out.println(myobject.getState());
+				variable = (myobject.getState().toString() == "TERMINATED") ? false : true;
+			}
+
+
+			System.out.println("this is the main thread it is running alright");
+
+
 			
 	}
 
 }
+
 
 
 class something extends Thread{
@@ -23,6 +42,13 @@ class something extends Thread{
 @Override
 public void run(){
 	System.out.println("This thread has been created,and is running");
+
+	try{
+	Thread.sleep(10000);
+	}
+	catch(Exception e){
+		System.out.println("An error occured");
+	}
 }
 
 
